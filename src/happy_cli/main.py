c7ca8fa089a2c151e79a9d201fcbff1430a538e3
sheldonldev <xiadanli0320@gmail.com>
2024-03-02 10:48:30 +0800

    optimize commands

--- src/happy_cli/main.py
+++ src/happy_cli/main.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-from importlib.metadata import version
 from pathlib import Path
 from typing import Optional
 
@@ -8,42 +7,43 @@ import inflection
 import toml
 import typer
 from rich import print as rprint
+from typing_extensions import Annotated
 
-__version__ = "0.1.2"
+__info__ = toml.loads(Path(__file__).parent.parent.parent.joinpath('pyproject.toml').read_text())
+__version__ = __info__['project']['version']
 
 
 def version_callback(value: bool):
     if value:
-        typer.echo(f"Awesome Happy CLI Version: {__version__}")
+        rprint(f"Awesome Happy CLI Version: {__version__}")
         raise typer.Exit()
 
 
-app = typer.Typer(add_completion=False)
+app = typer.Typer(add_completion=True)
 
 
 @app.callback()
-def common(
+def call_version(
     ctx: typer.Context,
-    version: bool = typer.Option(
-        None,
-        "--version",
-        callback=version_callback,
-    ),
+    version: Annotated[
+        Optional[bool],
+        typer.Option("--version", "-v", callback=version_callback),
+    ] = None,
 ):
     pass
 
 
 @app.command('info')
 def show_info():
-    rprint("My happy cli.")
+    rprint(toml.dumps(__info__))
 
 
-@app.command("create-project")
+@app.command("create")
 def create_project(
     name: Optional[str] = typer.Argument(None),
 ):
     if name is None:
-        rprint("project name not given!")
+        rprint("name not given!")
         rprint("Exited!")
         return
     target = os.path.abspath(os.getcwd())
@@ -54,7 +54,7 @@ def create_project(
         return
     else:
         stubs_dir = Path(__file__).parent.joinpath('stubs')
-        template_dir = stubs_dir.joinpath('template-empty')
+        template_dir = stubs_dir.joinpath('template-default')
         toml_str = template_dir.joinpath('pyproject.toml').read_text()
         toml_cfg = toml.loads(toml_str)
         toml_cfg['project']['name'] = name
@@ -64,5 +64,5 @@ def create_project(
         toml_cfg['tool']['setuptools']['packages']['find']['include'] = [f'{name}*']
         template_dir.joinpath('pyproject.toml').write_text(toml.dumps(toml_cfg))
         subprocess.run(f"cp -r {template_dir} {project_dir}", shell=True)
-        subprocess.run(f"mv {project_dir}/src/project_name {project_dir}/src/{name}", shell=True)
+        subprocess.run(f"mv {project_dir}/src/name {project_dir}/src/{name}", shell=True)
         rprint(f"{project_dir} created successfully!")
