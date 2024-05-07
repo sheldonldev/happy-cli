794e50668f83ccd1033361bad8d0f4ccb5e0effa
sheldonldev <xiadanli0320@gmail.com>
2024-05-07 18:02:50 +0800

    fix author

--- src/jampy_cli/_cfg.py
+++ src/jampy_cli/_cfg.py
@@ -9,9 +9,11 @@ VERSION = __info__.get('version')
 
 AUTHOR_NAME = None
 AUTHOR_EMAIL = None
-authors = __info__.get('authors')
-if isinstance(authors, list) and len(authors) > 0:
-    AUTHOR_NAME = authors[0].get('name')
-    AUTHOR_EMAIL = authors[0].get('email')
+author_email = __info__.get('author_email')
+if isinstance(author_email, str) and len(author_email) > 0:
+    if len(author_email.split()) > 1:
+        author, email = author_email.split()[:2]
+        AUTHOR_NAME = author
+        AUTHOR_EMAIL = email.lstrip('<').rstrip('>')
 
 STUBS_ROOT = Path(__file__).parent.joinpath('stubs')
