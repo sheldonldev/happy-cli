c3fe0ba45bb6fd39e942a95291b1705b5b9e3b98
sheldonldev <xiadanli0320@gmail.com>
2024-05-07 17:58:28 +0800

    fix author

--- src/jampy_cli/_cfg.py
+++ src/jampy_cli/_cfg.py
@@ -6,7 +6,12 @@ APP_NAME = 'util_common'
 __info__ = get_package_info(APP_NAME)
 
 VERSION = __info__.get('version')
-AUTHOR_EMAIL = __info__.get('author_email')
-AUTHOR_NAME = __info__.get('author_name')
+
+AUTHOR_NAME = None
+AUTHOR_EMAIL = None
+authors = __info__.get('authors')
+if isinstance(authors, list) and len(authors) > 0:
+    AUTHOR_NAME = authors[0].get('name')
+    AUTHOR_EMAIL = authors[0].get('email')
 
 STUBS_ROOT = Path(__file__).parent.joinpath('stubs')
