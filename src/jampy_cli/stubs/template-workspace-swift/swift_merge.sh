811d9c4c2fc6f65e258adbe3b5084d88943b2ae6
sheldon.lee <sheldon.lee@yunlsp.com>
2024-09-18 15:19:34 +0800

    delete-swift

--- src/jampy_cli/stubs/template-workspace-swift/swift_merge.sh
+++
@@ -1,7 +0,0 @@
-prev_ckpt=v5/checkpoint-200-merged
-curr_ckpt=v6/checkpoint-200
-
-CUDA_VISIBLE_DEVICES=0 swift export \
-    --model_id_or_path $(pwd)/models/mini_cpm_v_v2_0/output/$prev_ckpt \
-    --ckpt_dir $(pwd)/models/mini_cpm_v_v2_0/output/$curr_ckpt \
-    --merge_lora true
