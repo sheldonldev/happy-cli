e3729b16e3120b5c34ea2e8e018dbdb2e68e6e9c
sheldon.lee <sheldon.lee@yunlsp.com>
2024-09-23 15:18:29 +0800

    fix

--- src/jampy_cli/stubs/template-project-fastapi/src/name/app.py
+++ src/jampy_cli/stubs/template-project-fastapi/src/name/app.py
@@ -1,9 +1,9 @@
 from contextlib import asynccontextmanager
 
+from decompress_archive import _cfg
+from decompress_archive._log import log
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from temp_project import _cfg
-from temp_project._log import log
 from util_common.uuid import UUID
 
 
@@ -13,27 +13,25 @@ class CustomFastAPI(FastAPI):
         self.log = log
         self.uuid_generator = UUID()
 
+        self.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Starting up resources...")
-
+async def lifespan(app: CustomFastAPI):
+    app.log.info("Starting up resources...")
     for name in dir(_cfg):
         attr = getattr(_cfg, name)
         if name.isupper() and not callable(attr):
             print(f"{name}: {attr}")
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
     yield
-
-    print("Shutting down resources...")
+    app.log.info("Shutting down resources...")
 
 
 app = CustomFastAPI(docs_url=None, redoc_url=None, lifespan=lifespan)
